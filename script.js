3f4da541e05c56f03c9445daadb557eeb5368810
Gabriel Stroe-ubuntu <gabrielstroe09@gmail.com.com>
2021-05-26 19:03:21 +0200

    darkmode

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
-function darkmode() {
-  var body = document.body;
-  body.classList.toggle("dark-mode");
+let darkmode = localStorage.getItem("darkMode");
+
+const toggleDarkMode = document.querySelector(".circle");
+
+const enableDarkMode = () => {
+  document.body.classList.add("dark-mode");
+
+  localStorage.setItem("darkMode", "enabled");
+};
+
+const disableDarkMode = () => {
+  document.body.classList.remove("dark-mode");
+
+  localStorage.setItem("darkMode", null);
+};
+
+if (darkmode === "enabled") {
+  enableDarkMode();
 }
+
+toggleDarkMode.addEventListener("click", () => {
+  darkmode = localStorage.getItem("darkMode");
+
+  if (darkmode !== "enabled") {
+    enableDarkMode();
+  } else {
+    disableDarkMode();
+  }
+});
